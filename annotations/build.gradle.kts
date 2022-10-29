916ac2d1489b49355668ec611dd1f48e0f813b2c
james <james-gibson@ntlworld.com>
2022-10-29 11:18:57 +0100

    Fixing dependencies

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
     `maven-publish`
 }
 dependencies {
-    implementation("com.google.auto.service:auto-service:1.0.1")
+    api("com.google.auto.service:auto-service:1.0.1")
     kapt("com.google.auto.service:auto-service:1.0.1")
-    implementation("com.squareup:kotlinpoet:1.12.0")
-    implementation("com.squareup:kotlinpoet-metadata:1.12.0")
+    api("com.squareup:kotlinpoet:1.12.0")
+    api("com.squareup:kotlinpoet-metadata:1.12.0")
     implementation(project(":core"))
-    implementation("com.natpryce:konfig:1.6.10.0")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.amshove.kluent:kluent:1.68")
+    api("com.natpryce:konfig:1.6.10.0")
+    testApi("junit:junit:4.13.2")
+    testApi("org.amshove.kluent:kluent:1.72")
     kaptTest(project(":annotations"))
 }
 afterEvaluate {
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 }
 
 dependencies {
-    implementation("redis.clients:jedis:4.2.3")
-    implementation("com.natpryce:konfig:1.6.10.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0")
-    testImplementation("org.amshove.kluent:kluent:1.68")
-    testImplementation("junit:junit:4.13.2")
+    api("redis.clients:jedis:4.3.1")
+    api("com.natpryce:konfig:1.6.10.0")
+    api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    testApi("org.amshove.kluent:kluent:1.72")
+    testApi("junit:junit:4.13.2")
     //testImplementation(kotlin("test"))
 
 }
