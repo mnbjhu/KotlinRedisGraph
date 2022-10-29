e20ac5efe1594200515e0df9f4fcef12c713d77a
james <james-gibson@ntlworld.com>
2022-10-29 20:47:11 +0100

    Refactoring shared code

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     api("redis.clients:jedis:4.3.1")
     api("com.natpryce:konfig:1.6.10.0")
     api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation(project(":annotations"))
     testApi("org.amshove.kluent:kluent:1.72")
     testApi("junit:junit:4.13.2")
     //testImplementation(kotlin("test"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 
 rootProject.name = "KotlinRedisGraph"
-include(":core", ":annotations")
+include(":core", ":processor", "annotations")
