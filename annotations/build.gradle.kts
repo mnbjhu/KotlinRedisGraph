fbdda78e345d554112492f64a88faf08f0f120f2
james <james-gibson@ntlworld.com>
2022-11-05 15:27:32 +0000

    In-progress

---
+++ annotations/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+
+}
+
+group = "uk.gibby"
+version = "0.8.8"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
